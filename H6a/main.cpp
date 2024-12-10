3a4c71bc072f0b8fbfb3d3e3a8f0ff3d26928b96
Santeri Syrjälä <santeri.syrjala@windowslive.com>
2024-12-10 15:27:27 +0200

    H6a

---
+++ H6a/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <myClass.h>
+
+using namespace std;
+
+int main()
+{
+    //luodaan olio luokasta myClass
+    MyClass objectClass;
+
+    objectClass.raiseMySignal();
+    return 0;
+}
+
+
